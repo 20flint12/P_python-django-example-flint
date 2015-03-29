c3c6b5893c257b70a51c3b0fa101ef1ae446e458
20flint12 <20flint12@gmail.com>
2015-03-29 20:44:48 +0300

    add view time

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,12 +2,28 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
+##############################################################
+import views
+# from polls.models import Poll
+# from my_tests import views
+# import my_tests.views
+##############################################################
+# from mysite.views import hello, current_datetime
+
+
 admin.autodiscover()
+
+
 
 
 urlpatterns = patterns(
     '',
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    ('^hello/$', views.hello),  ##############################
+    ('^time/$',  views.current_datetime),
 )
+
 urlpatterns += staticfiles_urlpatterns()
