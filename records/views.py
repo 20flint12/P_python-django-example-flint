f03bf8b2d900bf99f45b486b76a88b72320fe121
20flint12 <20flint12@gmail.com>
2015-06-06 22:44:18 +0300

    req cycle

--- records/views.py
+++ records/views.py
@@ -1,3 +1,9 @@
+
+import time
+import datetime
+
+
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -41,12 +47,9 @@ def search2(request):
     if 'q' in request.GET and request.GET['q']:
         q = request.GET['q' ]
         # books = Book.objects.filter(title__icontains=q)
-<<<<<<< HEAD
-        books = Publisher.objects.all()
-=======
+        # books = Publisher.objects.all()
         books = Book.objects.all()
         # books = Publisher.objects.all()
->>>>>>> ffba7e76e2c4ef73c326e96de8ccf8f4be1cf52f
         print books
         return render_to_response('records_search_results.html',
             {'books': books, 'query': q})
@@ -59,28 +62,83 @@ def search2(request):
 
 from records.models import RecNews
 
+import mysite.scrapes.scrape_data3 as scr
+import mysite.scrapes.scrape_data2 as scr2
+
+import multiprocessing as mp
+
+
+
 def news(request):
 
-    import datetime
+    # if my_proc_exec.is_alive():
+    #
+    #     pass
+    #
+    # else:
+    my_proc_exec = mp.Process(target=my_proc,
+                              args=(3,) )
+    my_proc_exec.start()
 
-
-    # Examples:
-    # time_stamp = today.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-    # format = "%b %d %H:%M:%S"
 
     dt = datetime.datetime.today()
     str_date_stamp = dt.strftime('%YYYY-%MM-%DD')
+    # print dt, str_date_stamp
 
+    ctx = scr2.get_temperature()
+    # ctx = scr.get_news()
 
-
-    # datetime.datetime.strptime("Jun-08-2013", '%b-%d-%Y').date()
-
-    # dt = datetime.strftime('%YYYY-%MM-%DD').date()
-    print dt
-    n1 = RecNews(news_date=dt, news_contents='asfghdahfa Telegraph Avenue')
+    n1 = RecNews(news_date=dt, news_contents=ctx)
     n1.save()
 
     books = RecNews.objects.all()
     print books
-    return render_to_response('records_search_results.html',
+    return render_to_response('news_search_results.html',
         {'books': books})
+
+
+
+
+
+def my_proc(repeat_counter):
+
+    # repeat = 1
+
+    begin_time = datetime.datetime.now()
+    print "\nBegin time:", str(begin_time)[:-7]
+    cur_time = begin_time
+    delta_time = datetime.timedelta(hours=0, minutes=20, seconds=30)
+    checkout_time = begin_time + delta_time
+
+    try:
+        while True:
+
+            # if repeat > repeat_counter:
+            #     break
+            #
+            # repeat += 1
+
+            if datetime.datetime.now() > checkout_time:
+
+                break
+                # Save full_str to file
+                checkout_time = datetime.datetime.now() + delta_time
+
+
+            dt = datetime.datetime.today()
+            # ctx = scr2.get_temperature()
+            ctx = scr.get_news()
+
+            n1 = RecNews(news_date=dt, news_contents=ctx)
+            n1.save()
+
+            print "+" * 100
+
+            # break
+
+            time.sleep(60)
+
+    except KeyboardInterrupt:
+
+        print '^C received, break'
+
