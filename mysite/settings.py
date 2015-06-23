32d0e11952197860805c85b3b3f57a0f22a70ce2
20flint12 <20flint12@gmail.com>
2015-06-23 17:53:24 +0300

    @#

--- mysite/settings.py
+++ mysite/settings.py
@@ -170,7 +170,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'gunicorn',
+
+    # 'gunicorn',
     'polls',
     'records',
 
