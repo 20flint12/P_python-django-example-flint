0762daf5ab7b4f0bd78116fcba27bb34a9f8fe9f
20flint12 <20flint12@gmail.com>
2015-06-24 11:26:00 +0300

    workers-2

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,8 +2,10 @@ import multiprocessing
 import os
 
 size = int(os.getenv("SIZE", 1))
+print "size=", size
+
 # Maximum 5 worker per 128MB
-workers = min(multiprocessing.cpu_count() * 2 + 1, size * 5)
+workers = min(multiprocessing.cpu_count() * 2 + 1 - 2, size * 5)
 timeout = 120
 
 print "workers=", workers
