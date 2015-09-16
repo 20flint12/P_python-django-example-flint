982a2858ac2d0f9fcb2501bbca7f2f4ba39a3c73
20flint12 <20flint12@gmail.com>
2015-09-16 12:24:51 +0300

    timeout half of the day

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -9,8 +9,8 @@ size = int(os.getenv("SIZE", 1))
 print "size=", size
 
 # Maximum 5 worker per 128MB
-# workers = min(multiprocessing.cpu_count() * 2 + 1, size * 5)
-workers = 4
-timeout = 120
+workers = min(multiprocessing.cpu_count() * 2 + 1, size * 5)
+# workers = 4
+timeout = 720
 
 print "workers=", workers
