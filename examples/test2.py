06de20b9c35ff8f3e443aa339f6a741bb287d6ee
20flint12 <20flint12@gmail.com>
2015-09-28 09:34:27 +0300

    test2

---
+++ examples/test2.py
@@ -0,0 +1,42 @@
+__author__ = 'sergey'
+
+
+def my_plot():
+
+    import random
+    import django
+    import datetime
+
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    # print "!" * 100
+
+    import matplotlib.pyplot as plt
+
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+    plt.subplot(212)
+    plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+    plt.show()
+
+
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=4)
+    for i in range(30):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+
+    canvas=FigureCanvas(fig)
+
