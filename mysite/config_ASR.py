bed81e11ba0ea22e07058bac3a65343705005cee
20flint12 <20flint12@gmail.com>
2016-01-28 09:35:57 +0200

    added subscibers

--- mysite/config_ASR.py
+++ mysite/config_ASR.py
@@ -35,14 +35,20 @@ EMAIL_SET = {
 
     "Kharkiv":
         [
-            '20flint12@gmail.com',
+            # '20flint12@gmail.com',
             '380688845064@sms.kyivstar.net',
+            #-------------------------------
+            '380688846025@sms.kyivstar.net',    # Инна
+            '380975135252@sms.kyivstar.net',    # Володя-планетарий
+            '380973497865@sms.kyivstar.net',    # Юра-планетарий
+
         ],
     "Kremenchug":
         [
-            # '20flint12@gmail.com',
-            '380688846025@sms.kyivstar.net',
-            '380678986620@sms.kyivstar.net',
+            '20flint12@gmail.com',
+            #-------------------------------
+            '380678986620@sms.kyivstar.net',    # мама
+            '380681546330@sms.kyivstar.net',    # тёща
         ],
     "Boston":
         [
