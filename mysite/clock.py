7a183df98c5a67db00dd55fe94532f183d4c7250
20flint12 <20flint12@gmail.com>
2016-03-01 19:49:47 +0200

    clock.py+300

--- mysite/clock.py
+++ mysite/clock.py
@@ -3,11 +3,11 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=300)
 def timed_job():
-    print('This job is run every three minutes.')
+    print('This job is run every three hundreds minutes.')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=5)
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
 
