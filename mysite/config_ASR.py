356342d6186ce99b7a2013538b324e5784763d0d
20flint12 <20flint12@gmail.com>
2016-03-12 21:15:23 +0200

    all subscribers: release

--- mysite/config_ASR.py
+++ mysite/config_ASR.py
@@ -39,15 +39,17 @@ EMAIL_SET = {
             '380688845064@sms.kyivstar.net',
             #-------------------------------
             '380688846025@sms.kyivstar.net',    # Инна
-            # '380975135252@sms.kyivstar.net',    # Володя-планетарий
-            # '380973497865@sms.kyivstar.net',    # Юра-планетарий
+            '380975135252@sms.kyivstar.net',    # Володя-планетарий
+            '380973497865@sms.kyivstar.net',    # Юра-планетарий
+
+            '380966140196@sms.kyivstar.net',    # Костя Нечаев
 
         ],
     "Kremenchug":
         [
             '20flint12@gmail.com',
             #-------------------------------
-            # '380678986620@sms.kyivstar.net',    # мама
+            '380678986620@sms.kyivstar.net',    # мама
             # '380681546330@sms.kyivstar.net',    # тёща
         ],
     "Boston":
