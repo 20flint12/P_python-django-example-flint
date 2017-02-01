afb155e374366b1d276704c0501ca9dafed6ddac
20flint12 <20flint12@gmail.com>
2017-02-01 19:52:54 +0200

    fixed tab hash

--- engine/static/engine/js/edit_place.js
+++ engine/static/engine/js/edit_place.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
 
 
+    // to "remember" selected tab
+    $("a[data-toggle='tab']").on("shown.bs.tab", function (e) {
+        var hash = $(e.target).attr("href");
+        if (hash.substr(0, 1) == "#") {
+            location.replace("#!" + hash.substr(1));
+        }
+    });
+
+
+    // to "restore" selected tab
+    if (location.hash.substr(0, 2) == "#!") {
+        $("a[href='#" + location.hash.substr(2) + "']").tab("show");
+    }
+
+
+
     $(function () {
         $('#datetimepicker12').datetimepicker({
             inline: true,
