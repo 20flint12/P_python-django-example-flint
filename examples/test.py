3e674e8935c1bd4fd51b5502aa4326d4149b84b7
20flint12 <20flint12@gmail.com>
2017-08-12 21:17:02 +0300

    matplot test

--- examples/test.py
+++ examples/test.py
@@ -19,5 +19,10 @@ import matplotlib.pyplot as plt
 # Initialize a Figure
 fig = plt.figure()
 
+
 # Add Axes to the Figure
-fig.add_axes([0,0,1,1])
+fig.add_axes([0, 0, 1, 1])
+
+plt.show()
+
+print(fig)
